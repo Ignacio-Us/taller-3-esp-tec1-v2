5ae7802ea6b3397ddb0da3c3ef49abfb615c28ce
C-RB <134893101+C-RB@users.noreply.github.com>
2024-06-06 16:50:17 -0400

    Creacion de la pague Login y de un archivo  JSON
    Se creó el login con los datos del usuario tales como gmail y la password. Además, se creó un JSON llamado 'db' con toda la información del usuario.

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,15 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import GamePage from "../pages/GamePage.vue";
 import SelectPage from "../pages/SelectPage.vue";
+import LoginPage from "../pages/LoginPage.vue";
 
 const routes = [
     { path: "/", component: HomePage},
     { path: "/game", component: GamePage},
     { path: "/selection", component: SelectPage},
+    { path: "/login", component: LoginPage}
 ];
+
 
 export const router = createRouter({
     history: createWebHistory(),
